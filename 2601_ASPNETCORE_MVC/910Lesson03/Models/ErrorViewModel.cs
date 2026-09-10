ddda1e8a75586d6f0bc2d702624fdaa7af699815
dangphi <naokhangduong@gmail.com>
2026-09-10 21:04:32 +0700

    Update 9/10

--- 2601_ASPNETCORE_MVC/910Lesson03/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace _910Lesson03.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
