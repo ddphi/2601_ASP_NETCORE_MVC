415aca6be80bfdc17737c1abd4652efc4627bb5c
dangphi <naokhangduong@gmail.com>
2026-09-17 19:30:02 +0700

    9/17 Lab4 - CRUD

---
+++ 2601_ASPNETCORE_MVC/Lab4_pdd/Controllers/PeopleController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Lab4_pdd.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab4_pdd.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: PeopleController
+        public ActionResult Index()
+        {
+            var _peoples = DataLocal.GetPeoples();
+            return View(_peoples);
+        }
+
+        // GET: Peoples/Details/5
+        public ActionResult Details(int id)
+        {
+            var peoples = DataLocal.GetPeopleById(id);
+            return View(peoples);
+        }
+
+        // GET: PeopleController/Create
+        public ActionResult Create()
+        {
+            People people = new People();
+            return View(people);
+        }
+
+        // POST: Peoples/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(People model)
+        {
+            try
+            {
+                var files = HttpContext.Request.Form.Files;
+                if (files.Count > 0 && files[0].Length > 0)
+                {
+                    var file = files[0];
+                    var fileName = file.FileName;
+                    var path = Path.Combine(Directory.GetCurrentDirectory(),
+                        "wwwroot\\images\\avatar", fileName);
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                        model.Avatar = "/images/avatar/" + fileName;
+                    }
+                }
+                model.Id = DataLocal._people.Max(x => x.Id) + 1;
+                DataLocal._people.Add(model);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ViewBag.error = ex.Message;
+                return View(model);
+            }
+        }
+
+        // GET: Peoples/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var people = DataLocal.GetPeopleById(id);
+            return View(people);
+        }
+
+        // POST: Peoples/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, People model)
+        {
+            try
+            {
+                var files = HttpContext.Request.Form.Files;
+                if (files.Count > 0 && files[0].Length > 0)
+                {
+                    var file = files[0];
+                    var fileName = file.FileName;
+                    var path = Path.Combine(Directory.GetCurrentDirectory(),
+                        "wwwroot\\images\\avatar", fileName);
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                        model.Avatar = "/images/avatar/" + fileName;
+                    }
+                }
+
+                var people = DataLocal.GetPeopleById(id);
+                if (people != null)
+                {
+                    people.Name = model.Name;
+                    people.Email = model.Email;
+                    people.Phone = model.Phone;
+                    people.Address = model.Address;
+                    if (!string.IsNullOrEmpty(model.Avatar))
+                        people.Avatar = model.Avatar;
+                    people.Birthday = model.Birthday;
+                    people.Bio = model.Bio;
+                    people.Gender = model.Gender;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ViewBag.error = ex.Message;
+                return View(model);
+            }
+        }
+
+        // GET: Peoples/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var peoples = DataLocal.GetPeopleById(id);
+            return View(peoples);
+        }
+
+        // POST: Peoples/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, People model)
+        {
+            try
+            {
+                for (int i = 0; i < DataLocal._people.Count; i++)
+                {
+                    if (DataLocal._people[i].Id == id)
+                    {
+                        DataLocal._people.RemoveAt(i);
+                        break;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ 2601_ASPNETCORE_MVC/Lab4_pdd/Models/DataLocal.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab4_pdd.Models
+{
+    public class DataLocal
+    {
+        public static List<People> _people = new List<People>()
+        {
+            new People() { Id = 0, Name = "Devmaster", Email = "devmaster.edu.vn@gmail.com",
+                Phone = "0978611889", Address = "25 Vũ Ngọc Phan",
+                Avatar = "/images/avatar/00.png", Birthday = Convert.ToDateTime("2012/09/22"),
+                Bio = "Viện Công Nghệ Devmaster", Gender = 0 },
+
+            new People() { Id = 1, Name = "Trinh Van Chung", Email = "chungtrinhj@gmail.com",
+                Phone = "0978611889", Address = "25 Vũ Ngọc Phan",
+                Avatar = "/images/avatar/01.jpg", Birthday = Convert.ToDateTime("1979/05/25"),
+                Bio = "Devmaster Academy", Gender = 1 },
+
+            new People() { Id = 2, Name = "Nguyễn Huy", Email = "huynhnguyen@gmail.com",
+                Phone = "0912113113", Address = "Gia lâm, hà nội",
+                Avatar = "/images/avatar/02.jpg", Birthday = Convert.ToDateTime("1999/02/12"),
+                Bio = "Viện Devmaster", Gender = 1 },
+
+            new People() { Id = 3, Name = "Tiểu Long Nữ", Email = "longnutieu@gmail.com",
+                Phone = "0904001002", Address = "Ba đình, hà nội",
+                Avatar = "/images/avatar/03.jpg", Birthday = Convert.ToDateTime("2000/02/02"),
+                Bio = "Nhân vật trong phim kiếm hiệp", Gender = 2 },
+
+            new People() { Id = 4, Name = "Pikachu", Email = "chupika@gmail.com",
+                Phone = "0902114115", Address = "Quảng trung, hà đông",
+                Avatar = "/images/avatar/04.jpg", Birthday = Convert.ToDateTime("2001/01/01"),
+                Bio = "Hoạt hình Nhật Bản", Gender = 0 }
+        };
+
+        public static List<People> GetPeoples()
+        {
+            return _people;
+        }
+
+        public static People? GetPeopleById(int Id)
+        {
+            var people = _people.FirstOrDefault(x => x.Id == Id);
+            return people;
+        }
+    }
+}
